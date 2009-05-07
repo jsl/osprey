7189c38ccc18c29054e079da8902acc505dca25a
Justin S. Leitgeb <justin@phq.org>
2009-05-07 06:52:52 -0400

    Fix bug where Osprey conflicted with Rails class loading

--- lib/osprey.rb
+++ lib/osprey.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 require 'singleton'
 require 'curb'
-require 'CGI'
 require 'json'
 require 'sequel'
 require 'memcache'
 require 'ostruct'
+
+# Workaround for an issue where a Rails hack seems to try and redefine this class.
+# We assume that if a Rails CGI class is loaded, we're good to go and don't need
+# this dependency.
+unless defined?(CGI::Cookie)
+  require 'CGI'
+end
 
 lib_dirs =  [ 'core_ext', File.join('osprey', 'backend'), 'osprey' ].map do |d|
   File.join(File.dirname(__FILE__), d)
